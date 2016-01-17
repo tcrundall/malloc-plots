30d09c60748cea864a6e4169c8172a5fad9fc689
tcrundall <tim.crundall@gmail.com>
2016-01-17 15:08:48 +1100

    Make plot generator more versatile

--- plotGenerator.py
+++ plotGenerator.py
@@ -1,34 +1,26 @@
 import matplotlib.pyplot as plt
 
 with open('../moleResults/cache-thrash16size.txt', 'r') as f:
+  header = f.readline().strip().replace(' ','').upper().split('\t')
+#  print(header)
   array = []
+  for i in range(len(header)):
+    array.append([])
+#  print array
   for line in f:
-    array.append(line.strip().split('\t'))
+    #array.append(line.strip().split('\t'))
+    data = line.strip().split('\t')
+    for i in range(len(data)):
+      array[i].append(float(data[i]))
+
+# print array
+
+plt.plot(array[0], array[1], '--ro')
+plt.plot(array[0], array[2], '--yo')
+plt.plot(array[0], array[3], '--bo')
 
 
-
-print array[0]
-for i in range(len(array[0])) :
-  array[0][i] = array[0][i].strip().upper()
-
-for i in range(1, len(array)):
-  for j in range(len(array[0])):
-    array[i][j] = float(array[i][j])
-
-print "here"
-
-for line in array:
-  print line
-
-
-plt.plot([1,2,3,4,5,6,7,8], [array[1][1],array[2][1],array[3][1],array[4][1],array[5][1],array[6][1],array[7][1],array[8][1]], '--ro')
-
-plt.plot([1,2,3,4,5,6,7,8], [array[1][2],array[2][2],array[3][2],array[4][2],array[5][2],array[6][2],array[7][2],array[8][2]], '--yo')
-
-plt.plot([1,2,3,4,5,6,7,8], [array[1][3],array[2][3],array[3][3],array[4][3],array[5][3],array[6][3],array[7][3],array[8][3]], '--bo')
-
-
-
-plt.axis([1, 8, 0, 7])
+plt.axis([min(array[0]), max(array[0]), min(array[1] + array[2] + array[3]), 
+                                        max(array[1] + array[2] + array[3])])
 plt.show()
 
